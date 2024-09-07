e81ce1ad72a643c2b200abc0c787b24f4b58662a
desenvolvedorwebV <50739488+desenvolvedorwebV@users.noreply.github.com>
2024-09-07 20:59:28 -0300

    Update script.js

--- script.js
+++ script.js
@@ -187,8 +187,9 @@ const fases = {
         boxes: [{x:3, y:7}, {x:5, y:4}, {x:6, y:6}, {x:8, y:3}],
         destinations: [{x:3, y:5}, {x:5, y:5}, {x:8, y:5}, {x:8, y:8}],
         walls: [{x:2, y:4}, {x:2, y:5}, {x:2, y:6}, {x:2, y:7}, {x:2, y:8}, {x:2, y:9}, {x:3, y:4}, {x:3, y:9}, {x:4, y:1}, {x:4, y:2}, {x:4, y:3}, {x:4, y:4}, {x:4, y:9}, {x:5, y:1}, {x:5, y:7}, {x:5, y:8}, {x:5, y:9}, {x:6, y:1}, {x:6, y:8}, {x:7, y:1}, {x:7, y:2}, {x:7, y:4}, {x:7, y:8}, {x:7, y:9}, {x:8, y:2}, {x:8, y:4}, {x:8, y:9}, {x:9, y:2}, {x:9, y:7}, {x:9, y:8}, {x:9, y:9}, {x:10, y:2}, {x:10, y:3}, {x:10, y:4}, {x:10, y:5}, {x:10, y:6}, {x:10, y:7}]
-    },
-    fase17: {
+    }
+};
+/*fase17: {
         player: {x:10, y:10},
         boxes: [{x:20, y:20}],
         destinations: [{x:19, y:19}],
@@ -211,10 +212,8 @@ const fases = {
         boxes: [{x:20, y:20}],
         destinations: [{x:19, y:19}],
         walls: [{x:18, y:18}, {x:1, y:1}]
-    }
-};
-
-let faseAtual = 'fase17'; // Começa na fase 1
+    }*/
+let faseAtual = 'fase1'; // Começa na fase 1
 
 let player = { ...fases[faseAtual].player };
 let boxes = [...fases[faseAtual].boxes];
